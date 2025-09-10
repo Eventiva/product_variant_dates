b31134873ac8e7fd4ace02f93b72b88255502f74
Jonathan Stevens <jonathan@resnovas.com>
2025-09-11 00:57:38 +0100

    Add computed fields for sale period management in product template model
    Introduce new fields: sale_start_date, sale_end_date, is_sale_period_active, and sale_period_info to manage and display sale periods based on variant attributes. Implement corresponding computation methods to derive values from product variants, enhancing the product template's functionality.

--- models/product_template.py
+++ models/product_template.py
@@ -1,10 +1,79 @@
 # -*- coding: utf-8 -*-
 
-from odoo import api, fields, models
+from datetime import datetime, date
+from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError
 
 
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
+
+    # Computed fields that inherit from variant attribute values
+    sale_start_date = fields.Datetime(
+        string='Sale Start Date',
+        compute='_compute_sale_dates_from_variants',
+        store=True,
+        help='Date from which this template can be sold (inherited from variant attribute values).'
+    )
+    sale_end_date = fields.Datetime(
+        string='Sale End Date',
+        compute='_compute_sale_dates_from_variants',
+        store=True,
+        help='Date after which this template cannot be sold (inherited from variant attribute values).'
+    )
+    is_sale_period_active = fields.Boolean(
+        string='Sale Period Active',
+        compute='_compute_is_sale_period_active',
+        store=True,
+        help='True if the current date is within the sale period'
+    )
+    sale_period_info = fields.Char(
+        string='Sale Period Info',
+        compute='_compute_sale_period_info',
+        help='Human readable information about the sale period'
+    )
+
+    @api.depends('product_variant_ids.sale_start_date', 'product_variant_ids.sale_end_date')
+    def _compute_sale_dates_from_variants(self):
+        """Compute sale dates from variant attribute values."""
+        for template in self:
+            variants = template.product_variant_ids.filtered('is_sale_period_active')
+
+            if not variants:
+                template.sale_start_date = False
+                template.sale_end_date = False
+                continue
+
+            # Get dates from variants
+            start_dates = [v.sale_start_date for v in variants if v.sale_start_date]
+            end_dates = [v.sale_end_date for v in variants if v.sale_end_date]
+
+            # Use the earliest start date and latest end date (least restrictive for template)
+            template.sale_start_date = min(start_dates) if start_dates else False
+            template.sale_end_date = max(end_dates) if end_dates else False
+
+    @api.depends('sale_start_date', 'sale_end_date')
+    def _compute_is_sale_period_active(self):
+        """Compute whether the template is currently within its sale period."""
+        now = fields.Datetime.now()
+        for template in self:
+            if template.sale_start_date and template.sale_start_date > now:
+                template.is_sale_period_active = False
+            elif template.sale_end_date and template.sale_end_date < now:
+                template.is_sale_period_active = False
+            else:
+                template.is_sale_period_active = True
+
+    @api.depends('sale_start_date', 'sale_end_date')
+    def _compute_sale_period_info(self):
+        """Compute human readable sale period information."""
+        for template in self:
+            info_parts = []
+            if template.sale_start_date:
+                info_parts.append(_('Available from %s') % template.sale_start_date.strftime('%Y-%m-%d'))
+            if template.sale_end_date:
+                info_parts.append(_('Until %s') % template.sale_end_date.strftime('%Y-%m-%d'))
+            template.sale_period_info = ' | '.join(info_parts) if info_parts else ''
 
     def _get_possible_variants_sorted(self):
         """Override to filter out variants that are not within their sale period."""
